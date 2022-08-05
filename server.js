10a7f5badb9577621116a0592796f3a4a44cbb9c
Xian, Jacky <Jacky.Xian@blueshieldca.com>
2022-08-04 20:08:56 -0700

    path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+const path = require('path');
 const app = express();
 
 var storage = multer.diskStorage({
