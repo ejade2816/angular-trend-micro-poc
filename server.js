c1f915e23706d2bbf285ccad7860fa96c4cc62a9
Xian, Jacky <Jacky.Xian@blueshieldca.com>
2022-08-04 19:25:18 -0700

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const app = express();
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        console.log(process.env.FILE_UPLOAD_DESTINATION_PATH)
+        // cb(null, "C:/Users/jxian01/Projects/CAA/virus_chk_pending");
+    },
+    filename: function (req, file, cb) {
+        console.log(file)
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+app.use(cors("*"));
+app.use(express.json());
+
+app.use(express.static(__dirname + '/dist/angular-trend-micro-poc'));
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/angular-trend-micro-poc/index.html'));
+});
+
+app.post("/api/document-upload", upload.single("document"), function (req, res) {
+    res.status(200).json({ "status": "ok" });
+});
+
+app.listen(process.env.PORT || 3200);
+console.log("Listening: ", process.env.PORT || 3200);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-trend-micro-poc';
+    title = 'angular-trend-micro-poc';
+
+    file?: File;
+    fileName?: string;
+    fileSize?: number;
+    fileType?: string;
+    isLoading: boolean = false;
+
+    constructor(private http: HttpClient) { }
+
+    onFileSelected(event: any) {
+
+        const file: File = event.target.files[0];
+        if (file) {
+            console.log(file)
+            this.file = file
+            this.fileName = file.name
+            this.fileSize = file.size
+            this.fileType = file.type
+        }
+    }
+
+    onUpload() {
+        console.log("onUpload")
+
+        this.isLoading = true;
+        const formData = new FormData();
+        formData.append("document", this.file!);
+        this.http.post("/api/document-upload", formData).subscribe(res => {
+            console.log(res)
+            this.isLoading = false;
+        });
+    }
 }
